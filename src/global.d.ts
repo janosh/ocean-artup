990217d4c7600d11f337304ccfb75efd719bb89e
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-10-29 21:02:08 +0100

    swap GA for plausible, add eslint pre-commit hook

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,2 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="google.maps" />
-/// <reference types="gtag.js" />
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,14 +12,12 @@
 
   import Header from '../components/Header.svelte'
   import Footer from '../components/Footer.svelte'
-  import GoogleAnalytics from '../components/GoogleAnalytics.svelte'
 
   import type { Link } from '../types'
 
   export let nav: Link[]
 </script>
 
-<GoogleAnalytics />
 <Header {nav} />
 <main>
   <slot />
